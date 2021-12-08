0dff3c62d1a4d954f4bebe0845fa14550e712623
solodd <54677613+solodd@users.noreply.github.com>
2021-12-08 18:56:08 +0300

    Обновление View Controller
    Изменение в создании UIlabel

--- test_couner1/ViewController.swift
+++ test_couner1/ViewController.swift
@@ -1,20 +1,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var a = 0
-    let label = UILabel()
+    var counter = 0
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private func zeroing(){
+        counter = 0
+        label.text = "Counter = \(counter)"
+        let alert = UIAlertController  (title: "Warning", message:   "counter > 5", preferredStyle: .alert)
+        let okButton = UIAlertAction (title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        present(alert, animated: true, completion: nil)
+    }
     @objc func tappedButton() {
-        a += 1
-        label.text = "\(a)"
-        func zeroing(){
-            a = 0
-            label.text = "\(a)"
-            let alert = UIAlertController  (title: "Warning", message:   "a > 5", preferredStyle: .alert)
-            let okButton = UIAlertAction (title: "OK", style: .default, handler: nil)
-            alert.addAction(okButton)
-            present(alert, animated: true, completion: nil)
-        }
-        if a > 5 {zeroing()}
+        counter += 1
+        label.text = "Counter = \(counter)"
+        if counter > 5 {zeroing()}
     }
     
     override func viewDidLoad() {
@@ -23,14 +26,14 @@ class ViewController: UIViewController {
         
         let btn = UIButton(type: .custom) as UIButton
         btn.backgroundColor = .gray
-        btn.setTitle("+1", for: .normal)
+        btn.setTitle("Add +1", for: .normal)
         btn.frame = CGRect(x: 75, y: 100, width: 200, height: 100)
         btn.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
         self.view.addSubview(btn)
     
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 100)
         label.textColor = .black
-        label.text = "0"
+        label.text = "counter = \(counter)"
         self.view.addSubview(label)
     }
     
