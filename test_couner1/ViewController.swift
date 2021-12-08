bbe4c496002ace3aa94bcf9ef559a9e3407652ee
solodd <54677613+solodd@users.noreply.github.com>
2021-12-08 10:53:26 +0300

    Initial Commit

---
+++ test_couner1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_couner1
+//
+//  Created by Alex Solod on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
