c5a9b500b743d27ec8dac2ae124f9f279c1e6249
solodd <54677613+solodd@users.noreply.github.com>
2021-12-08 18:23:23 +0300

    1

--- test_couner1/ViewController.swift
+++ test_couner1/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  test_couner1
-//
-//  Created by Alex Solod on 08.12.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    var a = 0
+    let label = UILabel()
+    @objc func tappedButton() {
+        a += 1
+        label.text = "\(a)"
+        func zeroing(){
+            a = 0
+            label.text = "\(a)"
+            let alert = UIAlertController  (title: "Warning", message:   "a > 5", preferredStyle: .alert)
+            let okButton = UIAlertAction (title: "OK", style: .default, handler: nil)
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+        }
+        if a > 5 {zeroing()}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        let btn = UIButton(type: .custom) as UIButton
+        btn.backgroundColor = .gray
+        btn.setTitle("+1", for: .normal)
+        btn.frame = CGRect(x: 75, y: 100, width: 200, height: 100)
+        btn.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+        self.view.addSubview(btn)
+    
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 100)
+        label.textColor = .black
+        label.text = "0"
+        self.view.addSubview(label)
     }
-
-
+    
+    
+    
+    
 }
 
